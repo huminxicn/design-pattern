273fb6a6c9a0292323420ba3bd6a9c06a8d98250
胡敏喜 <huminxi@nongfenqi.com>
2019-01-11 10:06:48 +0800

    Chain of Responsibility pattern finished.

---
+++ src/com/chain/NoSupport.java
@@ -0,0 +1,19 @@
+package com.chain;
+
+/**
+ * 什么问题都不处理。
+ *
+ * @author hf huminxi2017@gmail.com
+ * @version on 2019/1/11 09:43
+ */
+public class NoSupport extends Support {
+
+    public NoSupport(String name) {
+        super(name);
+    }
+
+    @Override
+    protected boolean resolve(Trouble trouble) {
+        return false;
+    }
+}
---
+++ src/com/chain/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Chain of Responsibility 责任链模式。
+ *
+ * @author hf huminxi2017@gmail.com
+ * @version on 2019/1/11 09:26
+ */
+package com.chain;
