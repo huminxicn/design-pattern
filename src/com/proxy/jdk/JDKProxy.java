a716217acb0e60ee949b290e1dab632e7e280d37
huminxicn <huminxi2017@gmail.com>
2021-07-05 15:42:56 +0800

    1.调整 JDKProxy.java 逻辑。

--- src/com/proxy/jdk/JDKProxy.java
+++ src/com/proxy/jdk/JDKProxy.java
@@ -1,6 +1,12 @@
 package com.proxy.jdk;
 
 
+import sun.misc.ProxyGenerator;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
@@ -27,8 +33,21 @@ public class JDKProxy {
                 new InvocationHandler() {
                     @Override
                     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                        //生成的代理类已经继承了 java.lang.reflect.Proxy 类。
+                        System.out.println("proxy instanceof Proxy: " + (proxy instanceof Proxy));
+                        //将生成的代理类输入到 JDKProxy.class 文件中，再通过 IDEA 反编译 JDKProxy.class 文件。
+                        byte[] bytes = ProxyGenerator.generateProxyClass("proxy", target.getClass().getInterfaces());
+                        File file = new File("./JDKProxy.class");
+                        try {
+                            OutputStream outputStream = new FileOutputStream(file);
+                            outputStream.write(bytes);
+                            outputStream.flush();
+                            outputStream.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
                         System.out.println("JDK 动态代理开始");
-                        //反射机制调用目标对象的方法
                         Object returnVal = method.invoke(target, args);
                         System.out.println("JDK 动态代理结束");
                         return returnVal;
